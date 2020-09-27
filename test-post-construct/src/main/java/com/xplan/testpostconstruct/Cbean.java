1536d5b3265e17035b71ae6d38e792f76831d7b8
chengluchao <chengluchao@baiwang.com>
2020-09-27 17:24:36 +0800

    postConstuct演示

---
+++ test-post-construct/src/main/java/com/xplan/testpostconstruct/Cbean.java
@@ -0,0 +1,30 @@
+package com.xplan.testpostconstruct;
+
+/**
+ * ClassName: Cbean
+ * Description:
+ * date: 2020/9/27 3:53 下午
+ *
+ * @author chengluchao
+ * @since JDK 1.8
+ */
+public class Cbean {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
